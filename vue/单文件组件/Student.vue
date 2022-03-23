a345d9231f107090aeae19181c7659764d0e4f93
xsm-huang <1012973656@qq.com>
2022-03-23 16:17:19 +0800

    vue-cli

---
+++ vue/单文件组件/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>学生名称: {{name}}</h3>
+        <h3>学生年龄: {{age}}</h3>  
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'xsm',
+            age: '20',
+        };
+    },
+
+}
+</script>
