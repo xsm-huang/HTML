9201c604d7d7b40f10539d8df1f296a23ffca659
xsm-huang <1012973656@qq.com>
2022-03-29 23:33:54 +0800

    消息订阅与发布

---
+++ vue/vue_cli_test/06_src_消息订阅与发布/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="app">
+        <h1>{{ msg }}</h1>
+        <MySchool />
+        <MyStudent />
+    </div>
+</template>
+
+<script>
+// 引入组件
+import MySchool from './components/MySchool.vue';
+import MyStudent from './components/MyStudent.vue';
+
+export default {
+    name: 'App',
+    components: { MySchool, MyStudent },
+    data() {
+        return {
+            msg: 'hello',
+        };
+    },
+};
+</script>
+
+<style>
+.app {
+    background-color: antiquewhite;
+    padding: 10px;
+}
+</style>
