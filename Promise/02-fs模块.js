745ce78c87831ea5ec107b83ac1bdbc2eaa0743e
xsm-huang <1012973656@qq.com>
2022-03-31 15:51:58 +0800

    3.30

---
+++ Promise/02-fs模块.js
@@ -0,0 +1,31 @@
+const { log } = require('console');
+const fs = require('fs');
+
+/* // 回调函数形式
+fs.readFile('./resource/02.txt', (err, data) => {
+    // 如果出错 则抛出错误
+    if (err) throw err;
+
+    // 输出文件内容
+    console.log(data.toString());
+});
+ */
+
+// promise 形式
+let p = new Promise((resolve, reject) => {
+    fs.readFile('./resource/content.txt', (err, data) => {
+        // 如果出错
+        if (err) throw reject(err);
+        // 如果成功
+        resolve(data);
+    });
+});
+// 调用 then
+p.then(
+    (value) => {
+        console.log(value.toString());
+    },
+    (reason) => {
+        console.log(reason);
+    }
+);
