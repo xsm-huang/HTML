84d77930c37b9ce97332454919304f8bbf197c3b
xsm-huang <1012973656@qq.com>
2022-04-04 23:15:34 +0800

    插槽

---
+++ vue/vue_cli_test/src/components/Category.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="category">
+        <h3>{{ title }}</h3>
+        <slot>
+            <ul>
+                <li v-for="(item, index) in listData" :key="index">{{ item }}</li>
+            </ul>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VList',
+    props: ['listData', 'title'],
+};
+</script>
+
+<style scoped>
+.category {
+    float: left;
+    background-color: antiquewhite;
+    width: 200px;
+    height: 300px;
+}
+h3 {
+    text-align: center;
+    background-color: aquamarine;
+}
+</style>
