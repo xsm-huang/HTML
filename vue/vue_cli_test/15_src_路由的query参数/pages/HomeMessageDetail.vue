8c008ef481c0f30cb5191772d1b819df4e19f7b1
xsm-huang <1012973656@qq.com>
2022-04-07 21:25:34 +0800

    路由的query参数

---
+++ vue/vue_cli_test/15_src_路由的query参数/pages/HomeMessageDetail.vue
@@ -0,0 +1,14 @@
+<template>
+    <ul>
+        <li>消息编号: {{ $route.query.id }}</li>
+        <li>消息标题: {{ $route.query.title }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'HomeMessageDetail',
+};
+</script>
+
+<style></style>
