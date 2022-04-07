980f77048ff2180c94fed5495ec33e013cc1d78b
xsm-huang <1012973656@qq.com>
2022-04-07 22:29:29 +0800

    缓存路由组件

---
+++ vue/vue_cli_test/18_src_缓存路由组件/router/index.js
@@ -0,0 +1,40 @@
+// 引入组件
+import VAbout from '../pages/VAbout.vue';
+import VHome from '../pages/VHome.vue';
+import HomeNews from '../pages/HomeNews.vue';
+import HomeMessage from '../pages/HomeMessage.vue';
+import HomeMessageDetail from '../pages/HomeMessageDetail.vue';
+
+// 引入路由器
+import VueRouter from 'vue-router';
+
+// 创建并暴露一个路由器
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: VAbout,
+        },
+        {
+            path: '/home',
+            component: VHome,
+            children: [
+                {
+                    path: 'news',
+                    component: HomeNews,
+                },
+                {
+                    path: 'message',
+                    component: HomeMessage,
+                    children: [
+                        {
+                            name: 'detail',
+                            path: 'detail/:id/:title',
+                            component: HomeMessageDetail,
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+});
