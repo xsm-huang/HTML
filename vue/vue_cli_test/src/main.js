d8d9c0f641f1b909f637d73955524788f9adfd4a
xsm-huang <1012973656@qq.com>
2022-04-08 13:42:37 +0800

    element ui

--- vue/vue_cli_test/src/main.js
+++ vue/vue_cli_test/src/main.js
@@ -2,16 +2,15 @@
 import Vue from 'vue';
 // 引入 App 组件，它是所有组件的父组件
 import App from './App.vue';
-// 引入 VueRouter
-import VueRouter from 'vue-router';
-// 引入一个路由器
-import router from './router/index';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 // 关闭 Vue 的生产提示
 Vue.config.productionTip = false;
 
 // 应用插件
-Vue.use(VueRouter);
+Vue.use(ElementUI);
 
 // 创建 vm
 new Vue({
